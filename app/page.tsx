a0abf8ccb64e31d57700d26e3eb176bd4d4de3a7
gr1lzy <kolomoets02@gmail.com>
2024-08-01 20:59:53 +0300

    add categories for site

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import {Container, Title} from "@/components/shared";
+import {Categories, Container, Title} from "@/components/shared";
 
 export default function Home() {
   return <>
 
     <Container className="mt-5">
       <Title text="All Pizzas" size="lg" className="font-extrabold"/>
+
+      <Categories />
     </Container>
 
   </>;
