6d5325dd93e7388af0d39bc13b33cbee68ebe3ed
gr1lzy <kolomoets02@gmail.com>
2024-08-02 22:05:59 +0300

    Add PriceRange and update Checkbox cn

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Title} from "@/components/shared/title";
 import {FilterCheckbox} from "@/components/shared/filter-checkbox";
+import {Input} from "@/components/ui";
 
 interface Props {
     className?: string;
@@ -14,6 +15,14 @@ export const Filters: React.FC<Props> = ({ className }) => {
             <div className="flex flex-col gap-4">
                 <FilterCheckbox text="Can be collected" value="1" />
                 <FilterCheckbox text="News" value="2" />
+            </div>
+
+            <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+                <p className="font-bold mb-3">Price range</p>
+                <div className="flex gap-3 mb-5">
+                    <Input type="number" placeholder="0" min={0} max={1000} defaultValue={0} />
+                    <Input type="number" min={100} max={1000} placeholder="1000" />
+                </div>
             </div>
         </div>
     );
