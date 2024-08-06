0922d28e8032050a12a904506b533b22b24e7265
gr1lzy <kolomoets02@gmail.com>
2024-08-06 22:50:02 +0300

    some changes in product-card

--- components/shared/product-card.tsx
+++ components/shared/product-card.tsx
@@ -12,12 +12,12 @@ interface Props {
     className?: string;
 }
 
-export const ProductCart: React.FC<Props> = ({ id, name, price, imageUrl, className}) => {
+export const ProductCard: React.FC<Props> = ({ id, name, price, imageUrl, className}) => {
     return (
         <div className={className}>
             <Link href={`/product/${id}`}>
-                <div className="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
-                    <img className="w-[215px] h-[215px]" src={imageUrl} alt={name} />
+                <div className="flex justify-center p-6 bg-white rounded-lg h-[260px]">
+                    <img className="w-[250px] h-[210px]" src={imageUrl} alt={name} />
                 </div>
 
                 <Title text={name} size="sm" className="mb-1 mt-3 font-bold" />
@@ -32,7 +32,7 @@ export const ProductCart: React.FC<Props> = ({ id, name, price, imageUrl, classN
                         from <b>{price} $</b>
                     </span>
 
-                    <Button variant="secondary" className="text-base font-bold">
+                    <Button variant="secondary" className="text-base font-bold bg-blue-50">
                         <Plus size={20} className="mr-1" />
                         Add to Cart
                     </Button>
